2e6bf812782d3cf6243915ecfbaceea65f25b6f4
Prashanth6539 <98127984+Prashanth6539@users.noreply.github.com>
2023-12-05 08:47:48 +0000

    Update First.sh

--- First.sh
+++ First.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Enter a numner"read a
+echo "Enter a number"
+read a
 
-echo "Enter a numner"read b
+echo "Enter a number"
+read b
 
-var=$((a+b))echo $var
+var=$((a+b))
+echo $var
